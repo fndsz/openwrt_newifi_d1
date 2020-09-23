da9f37c6acf8b02d617bfc5fc9c6e922cdbc0aeb
fndsz <45010248+fndsz@users.noreply.github.com>
2020-09-23 15:32:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,38 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+#device_name='NEWIFI D1'
+#wifi_name='NEWIFI_D1
+#lan_ip='192.168.1.1'        # Lan Ip地址
+#utc_name='Asia\/Shanghai'   # 时区
+ 
+#修改机器名称
+#echo "设置主机名"
+#sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
+
+
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#echo "设置lan ip"
+#sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
+
+
+#修改默认时区
+#echo "修改时区"
+#sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+
+
+#echo "修改wifi名称"
+
+#sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+#echo "替换文件"
