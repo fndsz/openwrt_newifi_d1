2449fc5a9a08749b2745bd3619e2a6b7766bbc3b
fndsz <45010248+fndsz@users.noreply.github.com>
2022-03-01 19:20:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -30,7 +30,7 @@ rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # 更新
-./scripts/feeds update -a && ./scripts/feeds install -a
+# ./scripts/feeds update -a && ./scripts/feeds install -a
 
 ##########
 # Modify the version number
