0003640e43fb847d185fe7c6a32b2ea38aada2ac
fndsz <45010248+fndsz@users.noreply.github.com>
2022-04-20 01:18:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,11 +23,11 @@ sed -i 's/192.168.1.1/10.28.1.1/g' package/base-files/files/bin/config_generate
 # sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # 删除原主题包
-rm -rf package/luci/luci-theme-argonne
+rm -rf package/luci/luci-theme-argon
 # rm -rf openwrt/package/lean/luci-theme-netgear
 
 # 添加新的主题包
-git clone -b 21.02 https://github.com/kenzok78/luci-theme-argonne.git package/luci/luci-theme-argonne
+# git clone -b 21.02 https://github.com/kenzok78/luci-theme-argonne.git package/luci/luci-theme-argonne
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # 更新
