dc593f8df422b1461992edeeb9069460ebe07266
fndsz <45010248+fndsz@users.noreply.github.com>
2022-04-19 20:36:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,9 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+# Add a feed source 
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
