d61b6c7b765f46aa511f728d221f1be47bd475f1
fndsz <45010248+fndsz@users.noreply.github.com>
2022-08-30 14:03:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,7 +23,6 @@ sed -i '$a src-git lienol https://github.com/Lienol/openwrt-packages' feeds.conf
 #rm openwrt/package/lean/luci-theme-argon -rf #delete original argon theme
 #echo 'src-git ArgonTheme https://github.com/jerrykuku/luci-theme-argon.git' >> 'feeds.conf.default' #repleace with JerryKuKu’s Argon Theme
 #echo 'src-git ArgonThemeConfig https://github.com/jerrykuku/luci-app-argon-config.git' >> 'feeds.conf.default' #add JerryKuKu’s Argon Config Feature
-
 rm -rf ./feeds/luci/themes/luci-theme-argon.
 #rm -rf package/lean/luci-theme-argon  #删除原Argon主题
 #git clone -b master https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon #加入JerryKuKu’s Argon
