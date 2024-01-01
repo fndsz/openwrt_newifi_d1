ca3656be3edd9b48c79bb7c0f9ae8fab8fc32efc
fndsz <45010248+fndsz@users.noreply.github.com>
2024-01-01 21:35:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/10.28.1.1/g' package/base-files/files/bin/config_generate
 
 ###### Modify hostname
-sed -i 's/ImmortalWrt/NewifiD1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/NewifiD1/g' package/base-files/files/bin/config_generate
 
 ####### Modify the version number
 #sed -i "s/ImmortalWrt /Fndsz build from Lede $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
