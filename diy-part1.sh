aa3fed307c66b09b32087fa7f0e5f5c6d055e39f
fndsz <45010248+fndsz@users.noreply.github.com>
2024-01-19 12:27:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,10 +25,10 @@ sed -i '$a src-git lienol https://github.com/Lienol/openwrt-packages' feeds.conf
 ####### Replace with JerryKuKu’s Argon
 rm -rf openwrt/package/lean/luci-theme-argon #delete original argon theme
 rm -rf ./feeds/luci/themes/luci-theme-argon
-echo 'src-git ArgonTheme https://github.com/jerrykuku/luci-theme-argon.git' >> 'feeds.conf.default' #repleace with JerryKuKu’s Argon Theme
-echo 'src-git ArgonThemeConfig https://github.com/jerrykuku/luci-app-argon-config.git' >> 'feeds.conf.default' #add JerryKuKu’s Argon Config Feature
+#echo 'src-git ArgonTheme https://github.com/jerrykuku/luci-theme-argon.git' >> 'feeds.conf.default' #repleace with JerryKuKu’s Argon Theme
+#echo 'src-git ArgonThemeConfig https://github.com/jerrykuku/luci-app-argon-config.git' >> 'feeds.conf.default' #add JerryKuKu’s Argon Config Feature
 rm -rf package/lean/luci-theme-argon  #删除原Argon主题
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon #加入JerryKuKu’s Argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config #加入JerryKuKu’s Argon Config
+git clone -b 23 https://github.com/kenzok78/luci-theme-argone package/lean/luci-theme-argon #加入JerryKuKu’s Argon
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config #加入JerryKuKu’s Argon Config
 cd ..
 #cd ..
