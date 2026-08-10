d1cfe85c94ac3bc31af82f7e5c25f9685ab52fc1
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-10 18:54:32 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,19 +11,9 @@
 #
 
 # 修改默认IP
-sed -i 's/192.168.1.1/10.28.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名称
-sed -i 's/ImmortalWrt/NewifiD1/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/Mi-R3G/g' package/base-files/files/bin/config_generate
 
-# 修改默认wifi名称ssid为MIR3G
-#sed -i 's/ssid=OpenWrt/ssid=MIR3G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 开启MU-MIMO
-#sed -i 's/mu_beamformer=0/mu_beamformer=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-# 取消bootstrap为默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
-# 修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
