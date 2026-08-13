bf4a77afd8e00ff475c52b571b8edf89ac95cd4e
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-13 08:50:43 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,9 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
 
 # Add a feed source
 git clone https://github.com/fndsz/mmdvm-op package/mmdvm/mmdvm-openwrt
 git clone https://github.com/siwind/luci-app-wolplus package/wolplus
-git clone https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community package/luci-app-tailscale-community
