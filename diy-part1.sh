3f925d7acd0a35ec30f8a63c9549c3023dcd621b
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-14 17:49:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,7 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 git clone https://github.com/fndsz/mmdvm-op package/mmdvm/mmdvm-openwrt
